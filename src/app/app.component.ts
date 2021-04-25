fdcdff41bb085e44fd2ace876070598fb9e929a5
Yunus ÖLMEZ <yns4832@hotmail.com>
2021-04-25 17:28:04 +0300

    some test updates!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ynslmzGithubIo';
+  title = 'Welcome to ynslmz.github.io';
 }
