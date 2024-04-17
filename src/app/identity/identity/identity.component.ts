3566b91b544553268363d6c1dc2b41dc18ff5baa
Yunus ÖLMEZ <yns4832@gmail.com>
2024-04-18 00:56:54 +0200

    update v15

--- src/app/identity/identity/identity.component.ts
+++ src/app/identity/identity/identity.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./identity.component.scss'],
 })
 export class IdentityComponent implements OnInit {
-  constructor() {}
+  constructor() { }
   currentYear = new Date().getFullYear();
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  links = [
+    {
+      url: 'https://twitter.com/ynslmz',
+      icon: 'bi bi-twitter',
+    },
+    {
+      url: 'https://www.linkedin.com/in/ynslmz/',
+      icon: 'bi bi-linkedin',
+    },
+    {
+      url: 'https://www.instagram.com/yns.lmz/',
+      icon: 'bi bi-instagram',
+    },
+    {
+      url: 'https://stackoverflow.com/users/3129784/ynslmz',
+      icon: 'fab fa-stack-overflow',
+    },
+    {
+      url: 'https://github.com/ynslmz',
+      icon: 'bi bi-github',
+    },
+    {
+      url: 'mailto:yns4832@gmail.com',
+      icon: 'bi bi-envelope',
+    }
+  ]
 }
